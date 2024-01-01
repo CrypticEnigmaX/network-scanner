ca630ecaea6370d74e0302d30bc627946e5e6765
CrypticEnigmaX <sahil049923@gmail.com>
2024-01-01 15:46:28 +0530

    Initial commit

---
+++ port_scanner.py
@@ -0,0 +1,33 @@
+::import socket
+import termcolor
+
+# Created by CrypticEnigmax
+
+def scan(ip_addr, ports):
+    print(termcolor.colored('\nStarted Scanning For ' + str(ip_addr), 'red', attrs=['bold', 'underline']))
+    for port in range(1, ports + 1):
+        port_scan(ip_addr, port)
+
+def port_scan(ip_addr, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        sock.connect((ip_addr, port))
+        print(termcolor.colored("[+] Port opened: " + str(port), 'green'))
+        sock.close()
+    except socket.error:
+        pass
+
+# Display an introductory message with your name
+print(termcolor.colored("Welcome to the CrypticEnigmaX Port Scanner!", 'cyan', attrs=['bold']))
+print(termcolor.colored("This tool is developed by CrypticEnigmaX for scanning ports.", 'cyan'))
+
+targets = input("Enter the targets you want to scan [split by ,]: ")
+ports = int(input("Enter the number of ports: "))
+
+if ',' in targets:
+    print(termcolor.colored("Scanning Multiple Targets...", 'red', attrs=['bold', 'underline']))
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(), ports)
+else:
+    scan(targets, ports)
